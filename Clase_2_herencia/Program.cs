7c7e512bcad22072f4e331675eef964f3a699954
Var-Guz <davidvarguz.7@unitec.edu>
2025-01-26 01:30:51 -0600

    data

--- Clase_2_herencia/Program.cs
+++ Clase_2_herencia/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Clase_2_herencia;
+
+Vertebrado vertebrado = new Vertebrado();
+Invertebrado invertebrado = new Invertebrado();
+int opcion;
+
+Console.WriteLine("*************************");
+Console.WriteLine("**          menu          **");
+Console.WriteLine("*1 Animal Vertebrado*");
+Console.WriteLine("*2 Animal Invertebrado*");
+Console.WriteLine("*************************");
+
+opcion = Convert.ToInt32(Console.ReadLine());
+
+switch (opcion)
+{
+    case 1:
+        Console.WriteLine("Escriba el nombre:");
+        vertebrado.Nombre = Console.ReadLine();
+
+        Console.WriteLine("Escriba el COLOR:");
+        vertebrado.Color = Console.ReadLine();
+
+        Console.WriteLine("Escriba el Tamano:");
+        vertebrado.Tamano = Console.ReadLine();
+
+        Console.WriteLine("Escriba la familia:");
+        vertebrado.Nombre = Console.ReadLine();
+
+        break;
+
+}
+
+
