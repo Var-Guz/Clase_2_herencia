db83689cf22604f1e39e3214dc80d15ff2753815
Var-Guz <davidvarguz.7@unitec.edu>
2025-01-26 01:31:24 -0600

    father add

---
+++ Clase_2_herencia/Animal.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Clase_2_herencia
+{
+    public class Animal
+    {
+        public string? Nombre { get; set; }
+        public string? Color { get; set; }
+        public string? Tamano { get; set; }
+        public string? Familia { get; set; }
+
+
+    }
+}
