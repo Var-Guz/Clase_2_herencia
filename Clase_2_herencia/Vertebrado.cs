5776e996afa941ded208b2705908afeff64eb4dc
Var-Guz <davidvarguz.7@unitec.edu>
2025-01-26 01:31:03 -0600

    vertebrados class

---
+++ Clase_2_herencia/Vertebrado.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Clase_2_herencia
+{
+    public class Vertebrado : Animal
+    {
+        public string Tipo { get; set; }
+        public bool TienePatas { get; set; }
+        public bool EsDomestico { get; set; }
+        public bool EsHerbivoro { get; set; }
+        public bool EsCarnivoro { get; set; }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Color: {Color}");
+            Console.WriteLine($"Tamano: {Tamano}");
+            Console.WriteLine($"Familia: {Familia}");
+            Console.WriteLine($"Tiene Patas: {(TienePatas ? "si" : "no")}");
+            Console.WriteLine($"Es Domestico: {(EsDomestico ? "si" : "no")}");
+            Console.WriteLine($"Es Hervivoro: {(EsHerbivoro ? "si" : "no")}");
+            Console.WriteLine($"Es Carnivoro: {(EsCarnivoro ? "si" : "no")}");
+        }
+
+        
+
+    }
+} 
