af81d69a37b028747d82af05bc4f727de48098e4
Var-Guz <davidvarguz.7@unitec.edu>
2025-01-26 01:31:12 -0600

    invertebrado class

---
+++ Clase_2_herencia/Invertebrado.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Clase_2_herencia
+{
+    internal class Invertebrado : Animal
+    {
+        public string? Tipo { get; set; }
+        public bool TienePatas { get; set; }
+        public int NumeroPatas { get; set; }
+        public bool TieneConcha { get; set; }
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Color: {Color}");
+            Console.WriteLine($"Tamano: {Tamano}");
+            Console.WriteLine($"Familia: {Familia}");
+            Console.WriteLine($"Tiene Patas: {(TienePatas ? "si" : "no")}");
+
+            if (TienePatas)
+            {
+                Console.WriteLine($"Tiene Patas: {TienePatas}");
+            }
+        }
+
+    }
+}
